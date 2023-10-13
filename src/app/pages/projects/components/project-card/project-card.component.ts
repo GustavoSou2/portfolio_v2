da41eea97188d9d46b5d086ae11258422e3ef34f
GustavoSou2 <gustavosocial1032@gmail.com>
2023-10-13 11:52:34 -0300

    feat (improvement): added option to change theme mode

--- src/app/pages/projects/components/project-card/project-card.component.ts
+++ src/app/pages/projects/components/project-card/project-card.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.scss']
+  styleUrls: ['./project-card.component.scss'],
 })
 export class ProjectCardComponent {
   @Input({ required: true }) project!: any;
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { of } from 'rxjs';
 import { projects } from './fake.db';
+import { itemByItemAnimation } from 'src/app/shared/animations/animations.global';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  animations: [itemByItemAnimation]
+
 })
 export class ProjectsComponent {
   
